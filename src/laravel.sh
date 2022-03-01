624d5ea8841d93d5a91c6cbd294924ac2c1df4e8
森山寛之 <>
2022-03-01 17:58:21 +0900

    公開

--- src/laravel.sh
+++ src/laravel.sh
@@ -1,13 +1,32 @@
 cd ../
+
+# 存在するフォルダを指定するとエラーになるため一度learningを削除
 rm -Rf ./learning
 
-composer create-project "laravel/laravel=${LARAVEL_VERSION}" learning
+# プロジェクト名 learning で作成
+composer create-project "laravel/laravel=6.*" learning -n
+
+# プロジェクトディレクトリに移動
 cd ./learning
+
+# .env ファイルを生成
 cp .env.example .env
+
+# keyを生成
 php artisan key:generate
+
+# storage 配下のシンボリックを作成
 php artisan storage:link
 
-sed -i -e "s/DB_HOST=127\.0\.0\.1/DB_HOST=db/g" \.env
-sed -i -e "s/DB_DATABASE=laravel/DB_DATABASE=$MYSQL_DATABASE/g" .env
-sed -i -e "s/DB_USERNAME=root/DB_USERNAME=$MYSQL_USER/g" .env
-sed -i -e "s/DB_PASSWORD=$/DB_PASSWORD=$MYSQL_PASSWORD/g" .env
+# storage配下に書き込み権限を付与
+chmod -R 777 /var/www/learning/storage
+
+# DB接続情報を上書き
+sed -i -e "s/DB_HOST=.*$/DB_HOST=db/" \.env
+sed -i -e "s/DB_DATABASE=.*$/DB_DATABASE=$MYSQL_DATABASE/" .env
+sed -i -e "s/DB_USERNAME=.*$/DB_USERNAME=$MYSQL_USER/" .env
+sed -i -e "s/DB_PASSWORD=.*$/DB_PASSWORD=$MYSQL_PASSWORD/" .env
+
+# DB接続チェックをします
+php artisan tinker ../db_check.php
+
